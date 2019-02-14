5cb33b09489c0edb1d41798d9f4bdf93b060a74f
CJ Kenney <ckenney422@gmail.com>
2019-02-14 11:00:36 -0600

    add useNewUrlParser to avoid warning

--- src/models/FeedModel.js
+++ src/models/FeedModel.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 let mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_HOST).then(() => {
+mongoose.connect(process.env.MONGO_HOST, { useNewUrlParser: true }).then(() => {
   console.log('Connected to database');
 });
 
